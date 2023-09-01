c83a37c4b65578f5071e21f9d09c50605419a15e
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-08-31 22:01:04 -0700

    load all data by api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log('index.js');
+const loadData = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+    const data = await res.json();
+    console.log(data.data);
+}
+loadData()
