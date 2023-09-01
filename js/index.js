23440ce7395a0b577e7cb3c7ecf415d8db51709d
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-01 09:06:07 -0700

    all category data show by default

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// loadData function here 
 const loadData = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
@@ -9,21 +10,28 @@ const loadData = async () => {
         div.innerHTML = `<button onclick="technologyDisplay('${technology.category_id}')" class="btn hover:bg-[#FF1F3D] hover:text-white">${technology.category}</button>`;
         technologiesBtnContainer.appendChild(div);
     });
+    technologyDisplay(technologies[0].category_id)
 }
+
+// technologyDisplay function here 
 const technologyDisplay = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
     const technologies = data.data;
     console.log(technologies);
     const technologiesContainer = document.getElementById('technologies-container');
+    technologiesContainer.textContent = '';
     technologies.forEach( technology => {
+        console.log(technology);
         const technologyCard = document.createElement('div');
         technologyCard.innerHTML = `
                         <div class="card  bg-base-100 shadow-xl p-4">
-                            <figure><img src="${technology.thumbnail}" alt="Shoes" /></figure>
+                            <figure><img src="${technology.thumbnail}" alt="technology-photo" /></figure>
                             <div class="card-body">
-                                    <h2 class="card-title">Shoes!</h2>
-                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                   <div>
+                                   <img src="" />
+                                   <p></p>
+                                   </div>
                                 <div class="card-actions justify-end">
                                     <button class="btn btn-primary">Buy Now</button>
                                 </div>
