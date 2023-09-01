61810903691eb7d154ff9d371071ea55bc6afa69
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-08-31 22:19:54 -0700

    added all technology btn and responsive for all devices

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const loadData = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
-    console.log(data.data);
+    const technologies = data.data;
+    const technologiesBtnContainer = document.getElementById('technologies-btn-container');
+    technologies.forEach( technology => {
+        console.log(technology);
+        const div = document.createElement('div');
+        div.innerHTML = `<button class="btn hover:bg-[#FF1F3D] hover:text-white">${technology.category}</button>`;
+        technologiesBtnContainer.appendChild(div);
+    });
 }
 loadData()
